a3b7d43e0d2cf815b9aa139a6718ddaead0032a6
Javier <javier_garber@yahoo.es>
2020-09-12 17:53:04 +0200

    Updates

---
+++ precache-manifest.fa5436458fcca366e3f1bb6d4abae2d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae30181ff1683296b79705d46367ebb",
+    "url": "/javiergarber/xanastur_concept/index.html"
+  },
+  {
+    "revision": "56581595f25ba1af8112",
+    "url": "/javiergarber/xanastur_concept/static/css/main.a43f1597.chunk.css"
+  },
+  {
+    "revision": "4f0207b36689ae0b07ee",
+    "url": "/javiergarber/xanastur_concept/static/js/2.a8fde1ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/javiergarber/xanastur_concept/static/js/2.a8fde1ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56581595f25ba1af8112",
+    "url": "/javiergarber/xanastur_concept/static/js/main.40e96921.chunk.js"
+  },
+  {
+    "revision": "b37ac460a0d1fabf25a1",
+    "url": "/javiergarber/xanastur_concept/static/js/runtime-main.e81bcf65.js"
+  }
+]);
